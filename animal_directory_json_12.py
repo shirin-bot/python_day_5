7a66b9ccb3e1a9963758b3a92ac0efce561c9061
shirin <mjshirin7@gmail.com>
2025-09-28 18:45:08 +0330

    animal_directory_json.py

---
+++ animal_directory_json_12.py
@@ -0,0 +1,41 @@
+import json
+try:
+    with open("animal.json","r") as f:
+        animal=json.load(f)
+except FileNotFoundError:
+    animal={}
+def save():
+    with open("animal.json","w") as f:
+        json.dump(animal,f,indent=4)
+def add():
+    x=input("name:")
+    z=input("domestic or wild? ")
+    animal[x]=z
+    print("added")
+    save()
+def show():
+    for a in animal:
+        print(a,animal[a])
+        
+def search():
+    n=input("name:")
+    if n in animal:
+        print(animal[n])
+    else:
+        print("not found")
+
+while True:
+    y=input("want do you wanth?(search ,add ,show ,exit):")
+    if y =="add":
+        add()
+    elif y=="search":
+        search()
+    elif y=="show":
+        show()
+    elif y=="exit":
+        print("end")
+    else:
+        print("Invalid option")
+        break
+    
+    
